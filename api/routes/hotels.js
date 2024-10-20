5162c67788b1f1f4548a054c68c4c3fe5bec3ad7
Argeem <sitthisakbannob@gmail.com>
2024-10-20 18:28:57 +0700

    Add prettier, Eslint and Hotel Model

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,18 +1,16 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
- 
-// create
-router.post('/',()=>{
-    
-})
 
-// update 
- 
+// create
+router.post('/', async () => {});
+
+// update
+
 // delete
 
 // get
- 
+
 // get all
 
-export default router
+export default router;
