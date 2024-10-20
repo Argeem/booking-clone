2e6cfb1f4302f6735aa9e2d2fc052255231caa10
Argeem <sitthisakbannob@gmail.com>
2024-10-20 17:59:02 +0700

    Initial project with module and model

---
+++ api/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import authRoute from './routes/auth.js';
+import userRoute from './routes/users.js';
+import hotelRoute from './routes/users.js';
+import roomRoute from './routes/rooms.js';
+
+const app = express();
+dotenv.config()
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB connected");
+})
+
+//middleware
+app.use("api/v1/auth", authRoute);
+app.use("api/v1/user", userRoute);
+app.use("api/v1/hotel", hotelRoute);
+app.use("api/v1/room", roomRoute);
+
+app.listen(8800, ()=>{
+    connect();
+    console.log('Server is running on port 8800');
+})
---
+++ api/routes/hotels.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+ 
+// create
+router.post('/',()=>{
+    
+})
+
+// update 
+ 
+// delete
+
+// get
+ 
+// get all
+
+export default router
