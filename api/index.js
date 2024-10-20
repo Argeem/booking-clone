fc53ae45158a3453b5547084eda193419af8c220
Argeem <sitthisakbannob@gmail.com>
2024-10-20 20:26:44 +0700

    Add hotel CRUD

--- api/index.js
+++ api/index.js
@@ -4,36 +4,37 @@ import mongoose from 'mongoose';
 
 import authRoute from './routes/auth.js';
 import userRoute from './routes/users.js';
-import hotelRoute from './routes/users.js';
+import hotelRoute from './routes/hotels.js';
 import roomRoute from './routes/rooms.js';
 
 const app = express();
-dotenv.config()
-
+dotenv.config();
 
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO);
-    } catch (error) {
-        throw error;
-    }
+  try {
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    throw error;
+  }
 };
 
-mongoose.connection.on("disconnected", () => {
-    console.log("MongoDB disconnected");
-})
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
 
-mongoose.connection.on("connected", () => {
-    console.log("MongoDB connected");
-})
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected');
+});
 
 //middleware
-app.use("api/v1/auth", authRoute);
-app.use("api/v1/user", userRoute);
-app.use("api/v1/hotel", hotelRoute);
-app.use("api/v1/room", roomRoute);
+app.use(express.json());
 
-app.listen(8800, ()=>{
-    connect();
-    console.log('Server is running on port 8800');
-})
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/hotel', hotelRoute);
+app.use('/api/v1/room', roomRoute);
+
+app.listen(8800, () => {
+  connect();
+  console.log('Server is running on port 8800');
+});
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,16 +1,68 @@
 import express from 'express';
+import Hotel from '../models/Hotel.js';
 
 const router = express.Router();
 
 // create
-router.post('/', async () => {});
+router.post('/', async (req, res) => {
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const saveHotel = await newHotel.save();
+    res.status(201).json(saveHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // update
+router.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  try {
+    const updateHotel = await Hotel.findByIdAndUpdate(
+      id,
+      { $set: body },
+      { new: true },
+    );
+    res.status(200).json(updateHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // delete
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await Hotel.findByIdAndDelete(id);
+    res.status(200).json('Hotel has been deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // get
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const hotel = await Hotel.findById(id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // get all
+router.get('/', async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 export default router;
